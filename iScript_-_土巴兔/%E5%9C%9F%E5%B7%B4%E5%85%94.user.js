50a25bfd62448da407d105f523b4463d10fbeaa2
Limin <zhulimin.china@gmail.com>
2014-05-17 23:33:24 +0800

    backup

--- iScript_-_土巴兔/%E5%9C%9F%E5%B7%B4%E5%85%94.user.js
+++ iScript_-_土巴兔/%E5%9C%9F%E5%B7%B4%E5%85%94.user.js
@@ -9,11 +9,12 @@ var is_close_window = false;
 
 ///////////////////////////////////////////////
 //
-// 定时全屏图片
+// 放大图片
 //
 ///////////////////////////////////////////////
-window.full_screen_show_img = function()
+window.full_screen_show_img = function(e)
 {
+  console.log(e.type);
   //console.log( 'full_screen_show_img entered' );
   
   var x = document.getElementById("show_img");
@@ -34,7 +35,7 @@ window.full_screen_show_img = function()
 
   if ( 0 == w || 0 == h ||  w == window_w || h == window_h )
   {
-    window.setTimeout( "full_screen_show_img()", TIMEOUT );
+    //window.setTimeout( "full_screen_show_img()", TIMEOUT );
     //console.log( "0 == w || 0 == h ||  w == window_w || h == window_h" );
     //console.log( 'full_screen_show_img exited' );
     return;
@@ -42,39 +43,47 @@ window.full_screen_show_img = function()
 
   var rate = w / h;
   var window_rate = window_w / window_h;
-  
-  x.style.position = "absolute";
-  x.style.margin = "0px 0px 0px 0px";
-  x.style.padding = "0px 0px 0px 0px";
-  
-  var enlarge = 0;
+  var new_w = window_w;
+  var new_h = window_h;
   
   // 横向全屏
   if ( window_rate < rate )
   {
-    enlarge = window_w / w;
+    new_h = h * (window_w / w);
   }
   // 纵向全屏
   else
   {
-    enlarge = window_h / h;
+    new_w = w * (window_h / h);
   }
   
-  var new_w = w * enlarge;
-  var new_h = h * enlarge;
-
-  x.style.width = new_w + 'px';
-  x.style.height = new_h + 'px';
-  x.style.left = (window_w - new_w ) / 2 + 'px';
-  x.style.top = (window_h - new_h ) / 2 + 'px';
+  x.style.position = "absolute";
+  x.style.margin = "0px 0px 0px 0px";
+  x.style.padding = "0px 0px 0px 0px";
+  x.style.width = new_w + "px";
+  x.style.height = new_h + "px";
+  x.style.left = (window_w - new_w ) / 2 + "px";
+  x.style.top = (window_h - new_h ) / 2 + "px";
   x.width = new_w;
   x.height = new_h;
   //console.log( 'full_screen_show_img - 全屏: width=' +  x.width + ', height=' + x.height ); 
 
-  window.setTimeout( "full_screen_show_img()", TIMEOUT );
+  //window.setTimeout( "full_screen_show_img()", TIMEOUT );
   //console.log( 'full_screen_show_img exited' );
 }
 
+window.addEventListener( "resize", full_screen_show_img, true );
+window.addEventListener( "load", full_screen_show_img, true );
+window.addEventListener( "pageshow", full_screen_show_img, true );
+window.addEventListener( "beforescriptexecute", full_screen_show_img, true );
+window.addEventListener( "afterscriptexecute", full_screen_show_img, true );
+window.addEventListener( "focus", full_screen_show_img, true );
+window.addEventListener( "blur", full_screen_show_img, true );
+document.addEventListener( "load", full_screen_show_img, true );
+document.addEventListener( "beforescriptexecute", full_screen_show_img, true );
+document.addEventListener( "afterscriptexecute", full_screen_show_img, true );
+document.addEventListener( "readystatechange", full_screen_show_img, true );
+document.addEventListener( "blur", full_screen_show_img, true );
 ///////////////////////////////////////////////
 //
 // 打开效果图列表 Ctrl/Shift + Left Mouse
@@ -126,6 +135,7 @@ function open_all_links(e)
   console.log( 'open_all_links exited' );
 }
 
+window.addEventListener("click", open_all_links, false);
 ///////////////////////////////////////////////
 //
 // 关闭窗口
@@ -143,19 +153,36 @@ function close_window()
 // 响应鼠标按下事件，如果是最后一张图片，则关闭窗口
 //
 ///////////////////////////////////////////////
+/*
+浏览器 	实现方法 	事件属性 	向上滚动 	向下滚动
+FireFox 	DOMMouseScroll 	detail 	-3 	3
+非FireFox 	onmousewheel 	wheelDelta 	120 	-120
+
+对于FireFox，这个变量是detail：滚轮向上滚动，detail=-3；向下滚动，detail=3。
+*/
 function close_window_on_last_page(e)
 {
-  console.log('close_window_on_last_page entered');
+  //console.log('close_window_on_last_page entered');
   
-  if ( e.button != 0 )
+  if ( e.type != 'load' ) // 处理 1/1 情况
   {
-    console.log('close_window_on_last_page exited');
-    return;
+    if ( e.type == 'mousedown' && e.button == 2 ) // 左0中1右2
+    {
+      //console.log('close_window_on_last_page mousedown button=' + e.button);
+      //console.log('close_window_on_last_page exited');
+      return;
+    }
+    else if ( "DOMMouseScroll" == e.type && e.detail != 3 )
+    {
+      //console.log('close_window_on_last_page - DOMMouseScroll detail=' + e.detail);
+      //console.log('close_window_on_last_page exited');
+      return;
+    }
   }
-  
+
   if ( is_close_window == true )
   {
-    console.log( 'closing window...' );
+    //console.log( 'closing window...' );
     close_window();
   }
   
@@ -163,40 +190,45 @@ function close_window_on_last_page(e)
   
   if ( x == null )
   {
-    console.log('close_window_on_last_page - x is null');
-    console.log('close_window_on_last_page exited');
+    //console.log('close_window_on_last_page - x is null');
+    //console.log('close_window_on_last_page exited');
     return;
   }
   
-  console.log('close_window_on_last_page - x.innerHTML=' + x.innerHTML);
+  //console.log('close_window_on_last_page - x.innerHTML=' + x.innerHTML);
   
   var page_info = x.innerHTML.split('/');
   
   if ( x.innerHTML.length < 3 || page_info.length != 2 )
   {
-    console.log('close_window_on_last_page - not match format x/x' );
-    console.log('close_window_on_last_page exited');
+    //console.log('close_window_on_last_page - not match format x/x' );
+    //console.log('close_window_on_last_page exited');
     return;
   }
   
   var cur_page = page_info[0];
   var all_page = page_info[1];
   
-  console.log( 'close_window_on_last_page - cur_page=' + cur_page + ', all_page=' + all_page );
+  //console.log( 'close_window_on_last_page - cur_page=' + cur_page + ', all_page=' + all_page );
   
   if ( cur_page == all_page )
   {
-    console.log( 'close_window_on_last_page - last page, will close on next mousedown' );
+    //console.log( 'close_window_on_last_page - last page, will close on next mousedown' );
     is_close_window = true;
   }
+  else
+  {
+    is_close_window = false;
+  }
   
-  console.log('close_window_on_last_page exited');
+  //console.log('close_window_on_last_page exited');
 }
 
+window.addEventListener("mousedown", close_window_on_last_page, false);
+window.addEventListener("mousedown", close_window_on_last_page, false);
+window.addEventListener("DOMMouseScroll", close_window_on_last_page, false);
 ////////////////////////////////////////////////////////////////////////////
 
-window.addEventListener('click', open_all_links, false);
-window.setTimeout( "full_screen_show_img()", TIMEOUT );
-window.addEventListener('mousedown', close_window_on_last_page, false);
-document.getElementById("dis").addEventListener( 'load', full_screen_show_img, false );
+
+
 
