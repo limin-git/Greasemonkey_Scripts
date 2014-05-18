6ecbb3a2c24bb5d29684c48c5031b96f20ae7740
Limin <zhulimin.china@gmail.com>
2014-05-18 13:25:29 +0800

    backup

--- iScript_-_土巴兔/%E5%9C%9F%E5%B7%B4%E5%85%94.user.js
+++ iScript_-_土巴兔/%E5%9C%9F%E5%B7%B4%E5%85%94.user.js
@@ -14,7 +14,7 @@ var is_close_window = false;
 ///////////////////////////////////////////////
 window.full_screen_show_img = function(e)
 {
-  console.log(e.type);
+  //console.log(e.type);
   //console.log( 'full_screen_show_img entered' );
   
   var x = document.getElementById("show_img");
@@ -24,6 +24,14 @@ window.full_screen_show_img = function(e)
      //console.log( "full_screen_show_img - x is null, URL=" + document.URL );
      //console.log( 'full_screen_show_img exited' );
      return;
+  }
+  
+  if (x.style.position != "absolute" || x.style.margin != "0px" || x.style.padding != "0px")
+  {
+    x.style.position = "absolute";
+    x.style.margin = "0px 0px 0px 0px";
+    x.style.padding = "0px 0px 0px 0px";
+    //console.log( "margin=%s, padding=%s, position=%s", x.style.margin, x.style.padding, x.style.position );
   }
 
   var w = x.width;
@@ -91,30 +99,30 @@ document.addEventListener( "blur", full_screen_show_img, true );
 ///////////////////////////////////////////////
 function open_all_links(e)
 {
-  console.log( 'open_all_links entered' );
-  console.log( 'open_all_links - button=' + e.button + ', ctrlKey=' + e.ctrlKey + ', shiftKey=' + e.shiftKey );
+  //console.log( 'open_all_links entered' );
+  //console.log( 'open_all_links - button=' + e.button + ', ctrlKey=' + e.ctrlKey + ', shiftKey=' + e.shiftKey );
   
   if ( e.button != 0 )
   {
-    console.log( 'open_all_links exited' );
+    //console.log( 'open_all_links exited' );
     return;
   }
   
   if ( !e.ctrlKey && !e.shiftKey )
   {
-    console.log( 'open_all_links exited' );
+    //console.log( 'open_all_links exited' );
     return;
   }
   
   // http://xiaoguotu.to8to.com/list0-24.html
   if ( document.URL.search( "/list" ) == -1 )
   {
-    console.log( 'open_all_links - cannot find /list in uRL: ' + document.URL );
-    console.log( 'open_all_links exited' );
+    //console.log( 'open_all_links - cannot find /list in uRL: ' + document.URL );
+    //console.log( 'open_all_links exited' );
     return;
   }
   
-  console.log( 'open_all_links - uRL=' + document.URL );
+  //console.log( 'open_all_links - uRL=' + document.URL );
   
   var x = document.getElementsByClassName( "cname" );
   
@@ -124,7 +132,7 @@ function open_all_links(e)
 
     if ( y[0].href )
     {
-      console.log( 'open_all_links - opening link: ' + y[0].href );
+      //console.log( 'open_all_links - opening link: ' + y[0].href );
       window.open( y[0].href );
       //var timeout = 100 * i;
       //console.log( 'open_all_links - opening link in ' + timeout + 'ms: ' + y[0].href );
@@ -132,7 +140,7 @@ function open_all_links(e)
     }
   }
   
-  console.log( 'open_all_links exited' );
+  //console.log( 'open_all_links exited' );
 }
 
 window.addEventListener("click", open_all_links, false);
@@ -160,24 +168,18 @@ FireFox 	DOMMouseScroll 	detail 	-3 	3
 
 对于FireFox，这个变量是detail：滚轮向上滚动，detail=-3；向下滚动，detail=3。
 */
-function close_window_on_last_page(e)
+function close_window_on_click_or_wheel(e)
 {
+  //console.trace(e.type);
   //console.log('close_window_on_last_page entered');
   
-  if ( e.type != 'load' ) // 处理 1/1 情况
+  if ( e.type == 'mousedown' && e.button == 2 ) // 右键单击 左0中1右2
   {
-    if ( e.type == 'mousedown' && e.button == 2 ) // 左0中1右2
-    {
-      //console.log('close_window_on_last_page mousedown button=' + e.button);
-      //console.log('close_window_on_last_page exited');
-      return;
-    }
-    else if ( "DOMMouseScroll" == e.type && e.detail != 3 )
-    {
-      //console.log('close_window_on_last_page - DOMMouseScroll detail=' + e.detail);
-      //console.log('close_window_on_last_page exited');
-      return;
-    }
+    return;
+  }
+  else if ( "wheel" == e.type && e.deltaY < 0 ) // scroll up
+  {
+    return;
   }
 
   if ( is_close_window == true )
@@ -185,48 +187,43 @@ function close_window_on_last_page(e)
     //console.log( 'closing window...' );
     close_window();
   }
-  
+}
+
+window.addEventListener("mousedown", close_window_on_click_or_wheel, true);
+window.addEventListener("wheel", close_window_on_click_or_wheel, true);
+
+function update_is_close_window()
+{
   var x = document.getElementById( "small_num" );
   
-  if ( x == null )
+  if ( null == x )
   {
-    //console.log('close_window_on_last_page - x is null');
-    //console.log('close_window_on_last_page exited');
     return;
   }
   
-  //console.log('close_window_on_last_page - x.innerHTML=' + x.innerHTML);
+  var info = x.innerHTML.split("/");
   
-  var page_info = x.innerHTML.split('/');
-  
-  if ( x.innerHTML.length < 3 || page_info.length != 2 )
+  if ( x.innerHTML.length < 3 || info.length != 2 )
   {
-    //console.log('close_window_on_last_page - not match format x/x' );
-    //console.log('close_window_on_last_page exited');
     return;
   }
   
-  var cur_page = page_info[0];
-  var all_page = page_info[1];
+  var cur = info[0];
+  var total = info[1];
   
-  //console.log( 'close_window_on_last_page - cur_page=' + cur_page + ', all_page=' + all_page );
-  
-  if ( cur_page == all_page )
+  if ( cur == total )
   {
-    //console.log( 'close_window_on_last_page - last page, will close on next mousedown' );
     is_close_window = true;
   }
   else
   {
     is_close_window = false;
   }
-  
-  //console.log('close_window_on_last_page exited');
+
+  //console.log( 'update_is_close_window - innerHTML=%s, cur=%s, total=%s, is_close_window=%d', x.innerHTML, cur, total, is_close_window );
 }
 
-window.addEventListener("mousedown", close_window_on_last_page, false);
-window.addEventListener("mousedown", close_window_on_last_page, false);
-window.addEventListener("DOMMouseScroll", close_window_on_last_page, false);
+document.addEventListener( "load", update_is_close_window, true );
 ////////////////////////////////////////////////////////////////////////////
 
 
